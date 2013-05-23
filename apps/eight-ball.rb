5928c633c779f5d00fc92873ddf4d4586e4d8809
Ramsey Nasser <ram@nas.sr>
2013-05-23 07:12:21 -0400

    Fix bugs in 8ball app

--- apps/eight-ball.rb
+++ apps/eight-ball.rb
@@ -1,10 +1,11 @@
-class 8Ball < Dumbstore::App
+class EightBall < Dumbstore::App
 	name 'Magic 8 Ball'
 	author 'Chris Gervang'
 	author_url 'http://gervangster.wordpress.com/'
 	description <<-DESCRIPTION
 	Ask the 8 ball a yes-no question, and an answer you shall receive. A good one to try is "Magic 8 Ball, will I win the lottery today?"
 	DESCRIPTION
+
 	text_id '8ball'
 
 	def text params
@@ -15,8 +16,8 @@ class 8Ball < Dumbstore::App
 			"Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", 
 			"My sources say no", "Outlook not so good", "Very doubtful"]
 		#mix em' up!
-		fotune.suffle!
-
+		fortune.shuffle!
+		
 		"<Response><Sms>#{fortune.first}</Sms></Response>"
 	end
 end
