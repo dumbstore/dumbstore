0d315d08bb62d0e60b14c583ea394a2cf2abc1d6
Antoine <antoine@cater2.me>
2013-05-23 15:45:22 -0700

    Get the stock price of a company (Yahoo Finance API)

---
+++ apps/stock.rb
@@ -0,0 +1,36 @@
+require 'net/http'
+require 'rexml/document'
+
+class Stock < Dumbstore::App
+	name 'Stock Prices'
+	author 'Antoine de Chevigne'
+	author_url 'http://github.com/antoinedc'
+	description <<-DESCRIPTION
+	Get the current stock price of a company (based on Yahoo! Finance API).<br />
+	Format: <code>stock [symbol]</code><br />
+	Example: <code>stock YHOO</code>
+	DESCRIPTION
+	text_id 'stock'
+	voice_id 'stock'
+
+	def stock params
+		symb = params['Body']
+		url = "http://download.finance.yahoo.com/d/quotes.json?f=l1&s=" + symb
+		price = Net::HTTP.get_response(URI.parse(url)).body.strip
+		p price
+		if price == "0.00"
+			"Symbol not found."
+		else
+			price
+		end
+
+	end
+
+	def text params
+		"<Response><Sms>#{stock(params)}</Sms></Response>"
+	end
+
+	def voice params
+		"<Response><Say voice='woman'>#{stock(params)}</Say></Response>"
+	end
+end
