663999e094de749665a0769140a4b80857af3778
David Huerta <huertanix@opentil.com>
2013-05-23 01:35:46 -0400

    One in six chances I am never gonna give you up, never gonna let you down

---
+++ apps/rickroulette.rb
@@ -0,0 +1,25 @@
+class Rickroulette < Dumbstore::App
+  name 'Rick Roulette'
+  author 'David Huerta'
+  author_url 'http://www.davidhuerta.me'
+  description <<-DESCRIPTION
+  Call Dumbstore, enter the rickroulette for the ID. Hold your phone up to your head for a 1 in 6 chance of getting rickrolled'
+  DESCRIPTION
+
+  voice_id 'rickroulette'
+  def voice params
+    chambers = Array.new(6)
+    # load one in
+    chambers[0] = 'rickroll'
+    # spin
+    chambers.shuffle!
+
+    if chambers.first.nil?
+      # click
+      "<Response><Say voice='woman'>Click.</Say></Response>"
+    else
+      # bang!
+      "<Response><Play>http://www.davidhuerta.com/lulz/rickroll.mp3</Play></Response>"
+    end
+  end
+end
