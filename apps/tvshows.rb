7c3432f5093a004722dfd7614bb56d9334cc865d
Hassan Shamim <hassan.shamim@borderjump.com>
2013-05-28 16:52:06 -0500

    added TV show listing app

---
+++ apps/tvshows.rb
@@ -0,0 +1,19 @@
+require 'nokogiri'
+require 'open-uri'
+require 'date'
+
+class Tvshows < Dumbstore::App
+  name 'TVShows'
+  author 'Hassan Shamim'
+  author_url 'https://github.com/hassanshamim'
+  description 'Displays a list of TV shows which have new episodes airing today.'
+
+  text_id 'shows'
+  def text params
+    doc = Nokogiri::HTML(open("http://www.sidereel.com/calendar"))
+    titles = doc.css(".current .show-title").map{|n| n.inner_text }
+
+    response = "New episodes for #{Date.today.strftime}:  " + titles.join(",  ")
+    response.to_sms
+  end
+end
