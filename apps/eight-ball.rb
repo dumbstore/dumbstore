05827e15612686645b265a75ff5ab81e66eec5e3
Chris Gervang <gervangster@live.com>
2013-05-29 22:46:48 -0700

    improved quotify and eight ball

--- apps/eight-ball.rb
+++ apps/eight-ball.rb
@@ -9,15 +9,27 @@ class EightBall < Dumbstore::App
 	text_id '8ball'
 
 	def text params
+		message_body = params['Body']
+		
+		if message_body == nil
+			error = "Try again, this time with feeling (and words)"
+			error.to_sms
+		end	
+
+		if message_body == "help"
+			helper = "Ask me a yes-no question, and an answer you shall recieve"
+			helper.to_sms
+		end
 		#source: wikipedia article on 8 ball
 		fortune = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely", 
 			"You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", 
 			"Signs point to yes", "Reply hazy, try again", "Ask again later", "Better not tell you now",
 			"Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", 
-			"My sources say no", "Outlook not so good", "Very doubtful"]
+			"My sources say no", "Outlook not so good", "Very doubtful", "No way, man", "Boat.", 
+			"Do you kiss your mother with that mouth?"]
 		#mix em' up!
 		fortune.shuffle!
 		
-		"<Response><Sms>#{fortune.first}</Sms></Response>"
+		fortune.first.to_sms
 	end
 end
