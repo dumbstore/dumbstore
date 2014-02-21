f0fbd54019ac29750cd057f12d02fad44ad635f2
David Huerta <huertanix@opentil.com>
2014-02-20 22:05:07 -0500

    linebreaks n stuff

--- apps/asciigram.rb
+++ apps/asciigram.rb
@@ -11,48 +11,48 @@ class Asciigram < Dumbstore::App
   def text params
     ascii_art = Array.new
     # Mu the cat
-    ascii_art.push(" .       . \r\n \
- |\-----/| \r\n \
- | o   o | \r\n \
->|==>v<==|<\r\n \
- \   ^   / \r\n \
-  ==(M)==  \r\n \
- /       \ \r\n \
+    ascii_art.push(" .       . \n
+ |\-----/| \n
+ | o   o | \n
+>|==>v<==|<\n
+ \   ^   / \n
+  ==(M)==  \n
+ /       \ \n
 ")
     # Rando pug
-    ascii_art.push("  __________  \r\n \
- / /  /\  \ \ \r\n \
-/ / ^ || ^ \ \ \r\n \
-\/|(O))((O)|\/\r\n \
-  | /(..)\ |  \r\n \
- /\/ /--\ \/\ \r\n \
-/  --------  \ \r\n \
+    ascii_art.push("  __________  \n
+ / /  /\\  \\ \\  \n
+/ / ^ || ^ \\ \\  \n
+\\/|(O))((O)|\\/  \n
+  | /(..)\ |      \n
+ /\\/ /--\\ \\/\\ \n
+/  --------  \\   \n
 ")
     # Latte art from barista at Glass Shop 
-    ascii_art.push("   _-----_   \r\n \
- - .._ _.. -      \r\n \
-/ (((\v/))) \     \r\n \
-|   \\\|//   |###\ \r\n \
-\    \Y/    /__/- \r\n \
- -    |    -      \r\n \
-  \"\"-----\"\"       \r\n \
+    ascii_art.push("   _-----_   \n
+ - .._ _.. -       \n
+/ (((\\v/))) \\    \n
+|   \\\\|//   |### \n
+\\    \\Y/    /__/-\n
+ -    |    -       \n
+  \"\"-----\"\"    \n
 ")
     # Phoenix sunset
-    ascii_art.push(".................\r\n \
-.................\r\n \
-  _| ** * **__   \r\n \
- |:|_ **_**/--\\ \r\n \
--|::.|*(_)*|--|_-\r\n \
-  ______  ______ \r\n \
- /__[]_L\\/__[]_L\\ \r\n \
------------------\r\n \
+    ascii_art.push(".................\n
+.................\n
+  _| ** * **__   \n
+ |:|_ **_**/--\\ \n
+-|::.|*(_)*|--|_-\n
+  ______  ______ \n
+ /__[]_L\\/__[]_L\\ \n
+-----------------\n
 ")
     # Duckface selfie
-    ascii_art.push("\r\n \
-_   _  __ \r\n \
-o \ o |* |\r\n \
-  <   |_m|\r\n \
-      / / \r\n \
+    ascii_art.push("\n
+_   _  __  \n
+o \\ o |* |\n
+  <   |_m| \n
+      / /  \n
 ")
 
     ascii_art.shuffle!
