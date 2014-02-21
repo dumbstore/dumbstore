5674e632cd188d1bf4870212de376542be1663ed
David Huerta <huertanix@opentil.com>
2014-02-20 21:41:07 -0500

    ugh linebreaks

--- apps/asciigram.rb
+++ apps/asciigram.rb
@@ -11,48 +11,48 @@ class Asciigram < Dumbstore::App
   def text params
     ascii_art = Array.new
     # Mu the cat
-    ascii_art.push(" .       . \r\n
- |\-----/| \r\n
- | o   o | \r\n
->|==>v<==|<\r\n
- \   ^   / \r\n
-  ==(M)==  \r\n
- /       \ \r\n
+    ascii_art.push(" .       . \r\n \
+ |\-----/| \r\n \
+ | o   o | \r\n \
+>|==>v<==|<\r\n \
+ \   ^   / \r\n \
+  ==(M)==  \r\n \
+ /       \ \r\n \
 ")
     # Rando pug
-    ascii_art.push("  __________  \r\n
- / /  /\  \ \ \r\n
-/ / ^ || ^ \ \ \r\n
-\/|(O))((O)|\/\r\n
-  | /(..)\ |  \r\n
- /\/ /--\ \/\ \r\n
-/  --------  \ \r\n
+    ascii_art.push("  __________  \r\n \
+ / /  /\  \ \ \r\n \
+/ / ^ || ^ \ \ \r\n \
+\/|(O))((O)|\/\r\n \
+  | /(..)\ |  \r\n \
+ /\/ /--\ \/\ \r\n \
+/  --------  \ \r\n \
 ")
     # Latte art from barista at Glass Shop 
-    ascii_art.push("   _-----_   \r\n
- - .._ _.. -      \r\n
-/ (((\v/))) \     \r\n
-|   \\\|//   |###\ \r\n
-\    \Y/    /__/- \r\n
- -    |    -      \r\n
-  \"\"-----\"\"       \r\n
+    ascii_art.push("   _-----_   \r\n \
+ - .._ _.. -      \r\n \
+/ (((\v/))) \     \r\n \
+|   \\\|//   |###\ \r\n \
+\    \Y/    /__/- \r\n \
+ -    |    -      \r\n \
+  \"\"-----\"\"       \r\n \
 ")
     # Phoenix sunset
-    ascii_art.push(".................\r\n
-.................\r\n
-  _| ** * **__   \r\n
- |:|_ **_**/--\  \r\n
--|::.|*(_)*|--|_-\r\n
-  ______  ______ \r\n
- /__[]_L\/__[]_L\ \r\n
------------------\r\n
+    ascii_art.push(".................\r\n \
+.................\r\n \
+  _| ** * **__   \r\n \
+ |:|_ **_**/--\\ \r\n \
+-|::.|*(_)*|--|_-\r\n \
+  ______  ______ \r\n \
+ /__[]_L\\/__[]_L\\ \r\n \
+-----------------\r\n \
 ")
     # Duckface selfie
-    ascii_art.push("\r\n
-_   _  __ \r\n
-o \ o |* |\r\n
-  <   |_m|\r\n
-      / / \r\n
+    ascii_art.push("\r\n \
+_   _  __ \r\n \
+o \ o |* |\r\n \
+  <   |_m|\r\n \
+      / / \r\n \
 ")
 
     ascii_art.shuffle!
